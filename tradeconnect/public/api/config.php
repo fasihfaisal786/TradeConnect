d6d61bccbd89cad723f87c57db2f6fb714360362
M.Fasih Faisal <muhammadfasihfaisal786@gmail.com>
2026-08-13 18:26:14 +0500

    Update config.php

--- tradeconnect/public/api/config.php
+++ tradeconnect/public/api/config.php
@@ -1,15 +1,15 @@
 <?php
-// Session sirf ek baar start karo
 if(session_status() === PHP_SESSION_NONE){
     session_start();
 }
-define('DB_HOST','sql205.infinityfree.com');
-define('DB_USER','if0_42620569');
-define('DB_PASS','akberfaisal786');
-define('DB_NAME','if0_42620569_XXX');
+define('DB_HOST', getenv('MYSQLHOST'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASS', getenv('MYSQLPASSWORD'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_PORT', getenv('MYSQLPORT'));
 
 function getDB(){
-    $c = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    $c = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     if($c->connect_error){
         http_response_code(500);
         die(json_encode(['error'=>'DB Error: '.$c->connect_error]));
@@ -20,4 +20,3 @@ function getDB(){
 header('Access-Control-Allow-Origin: *');
 header('Cache-Control: no-cache, no-store, must-revalidate');
 ?>
-
